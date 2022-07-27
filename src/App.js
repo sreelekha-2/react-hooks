0950388624c188f662482e1df6a60e8d243e20d7
sreelekha-2 <sreelekhakutikanti@gmail.com>
2022-07-27 11:23:08 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Hooks from './components/Hooks';
 import './style.css';
 
 export default function App() {
+  const [isShow, setShow] = useState(true);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <Hooks />
+      <button onClick={() => setShow(!isShow)}>Toggle</button>
+
+      {isShow && <Hooks />}
     </div>
   );
 }
--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -6,17 +6,36 @@ export default function Hooks() {
   //componentDidMount and componentDidUpdate
   //it will fired on mount and update when state is updated
   useEffect(() => {
-    console.log('Mount and update');
+    console.log(' fired on mount and update');
   });
 
   //componentDidMount
   //it will fired only on mounting
   useEffect(() => {
-    console.log('Mount');
+    console.log('fired only on mounting');
   }, []);
+
+  //componentDidUpdate
+  //it will fired only on update
+  useEffect(() => {
+    console.log('fired only on update');
+  }, [count]);
+
+  //componentWillUnmount and componentDidUpdate
+  //it will fired when component unmounted means removed and updated
+  // useEffect(() => {
+  //   return function () {
+  //     console.log('fired unmounted and updated');
+  //   };
+  // });
 
   //componentWillUnmount
   //it will fired when component unmounted means removed
+  useEffect(() => {
+    return function () {
+      console.log('only on unmounted');
+    };
+  }, []);
 
   const incrementCount = () => {
     setCount(count + 1);
